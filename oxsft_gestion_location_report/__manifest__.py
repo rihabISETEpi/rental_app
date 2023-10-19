75ade1c10048fe67fe2b03f17bf954c925af95b7
Rihab-CH <chtabra.rihab@gmail.com>
2023-10-19 11:49:02 +0100

    Rental apps : V04

--- oxsft_gestion_location_report/__manifest__.py
+++ oxsft_gestion_location_report/__manifest__.py
@@ -25,7 +25,7 @@
     'category': 'Management',
     'website' : '',
     'author': 'OXYSOFT',
-    'depends': [],
+    'depends': [] ,
     'data': [
             'views/vehicle_cost_report_view.xml',
             'security/ir.model.access.csv',
